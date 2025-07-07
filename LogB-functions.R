e804ac22705baefe269e55018df9d693fdfff3b9
Joaquim Silvestre Reis <169111594+JoaquimReis@users.noreply.github.com>
2025-07-07 08:34:29 -0300

    LogB-functions.R

--- LogB-functions.R
+++ LogB-functions.R
@@ -30,24 +30,37 @@ pLB <- function(y, mu = 0.75) {
 # integrate(dLB, 0, .77) 
 
 
-# ---------------------------------------------------------------------------
-# FUNÇÃO INVERSA PARA GERAÇÃO DE NÚMEROS ALEATÓRIOS
-# ---------------------------------------------------------------------------
-
+# Função inversa (quantílica) Log-Bilal via método da inversão numérica
+#------------------------------------------------------------------------------------------
 inverse <- function(f, lower, upper){
   function(y){
-    uniroot(function(x){f(x) - y}, lower = lower, upper = upper, tol=1e-10)$root
+    uniroot(function(x){f(x) - y}, lower = lower, upper = upper, tol = 1e-10)$root
   }
 }
 
-qLB <- function(u,mu){
+qLB <- function(u, mu){
+  if (any(u <= 0 | u >= 1)) stop(paste("u must be between 0 and 1", "\n", ""))
+  
   f<- function(y) {3 * y^(4 / ((mu / (mu + 24))^(-1/2) - 5)) - 2 * y^(6 / ((mu / (mu + 24))^(-1/2) - 5))
   }
   q<-inverse(f, lower = 0, upper = 1)
   return(q(u))
 }
 
-# y=.6
+# y=.7
 # mu=.15
 # u=pLB(y,mu)
 # qLB(u,mu)
+
+#------------------------------------------------------------------------------------------
+# Função de geração de números aleatórios - Distribuição Log-Bilal
+#------------------------------------------------------------------------------------------
+rLB <- function(n, mu) {
+  u <- runif(n)
+  y <- mapply(function(ui, mui) qLB(ui, mu = mui), u, mu)
+  return(y)
+}
+
+# Exemplo de uso:
+# y <- rLB(1000, mu = 0.6)
+# hist(y, breaks = 30, main = "Amostras da distribuição Log-Bilal")
